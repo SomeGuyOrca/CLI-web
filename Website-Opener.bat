3ecbd151fa5138bac32cb3eaaeef74ad9ceb36b7
Name.txt <96922863+SomeGuyOrca@users.noreply.github.com>
2023-04-29 11:25:20 +0530

    Delete Website-Opener.bat

--- Website-Opener.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo browser
-set /p website=Enter the name of the websirte (Eg. youtube):
-set /p 2domain=Enter the secondary domain of the website (optional. eg. pages.GITHUB):
-set /p domain=Enter the domain of the website (eg. net):
-
-start http://%website%.%domain%
