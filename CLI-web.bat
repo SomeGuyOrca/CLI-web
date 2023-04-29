1d2a5f4b4c2b7ff79c7c1bc7c2bc39b765cea168
Name.txt <96922863+SomeGuyOrca@users.noreply.github.com>
2023-04-29 11:46:02 +0530

    Update CLI-web.bat

--- CLI-web.bat
+++ CLI-web.bat
@@ -1,26 +1,33 @@
 @echo off
 @title CLI-web
-echo browser
+echo 						welcome to CLI-web
 set /p web=Enter where you want to go search engine(bing) or a website itself (1,2): 
 
-if %web% == 1 goto bing
+if %web% == 1 goto ask
 if %web% == 2 goto website
-:bing
+:ask
 set /p enter=Enter the search engine duckduckgo, google, bing (1,2,3): 
 if %enter% == 1 goto ddg
 if %enter% == 2 goto google
 if %enter% == 3 goto bing
 
 :ddg
-start https://duckduckgo.com/?q=%enter%
-
+set /p query=Enter what you want to search for: 
+start https://duckduckgo.com/?q=%query%
+cls
+goto ddg
 :google
-start https://www.google.com/search?q=%enter%
-
+set /p query=Enter what you want to search for
+start https://www.google.com/search?q=%query%
+cls
+goto google
 :bing
-start https://www.bing.com/search?q=%enter%
-
+set /p query=Enter what you want to search for
+start https://www.bing.com/search?q=%query%
+cls
+goto bing
 :website
-set /p website=Enter the name of the website (Eg. youtube):
+set /p webpage=Enter the name of the website (Eg. youtube):
 set /p domain=Enter the domain of the website (eg. net):
-start http://%website%.%domain%
+start http://%webpage%.%domain%
+goto website
